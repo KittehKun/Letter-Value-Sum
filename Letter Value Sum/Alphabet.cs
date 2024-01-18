91ec2ad3371692097d5c4ac514700d8d0e1230a3
Zach H <huntzachary30@gmail.com>
2024-01-17 19:47:01 -0600

    Add project files.

---
+++ Letter Value Sum/Alphabet.cs
@@ -0,0 +1,42 @@
+﻿namespace Letter_Value_Sum
+{
+    internal class Alphabet
+    {
+        //A dictionary to store the alphabet and their corresponding values - capital letters are counted as the same value as lowercase letters
+        private static readonly Dictionary<char, int> _alphabet = new Dictionary<char, int>()
+        {
+            {'a', 1},
+            {'b', 2},
+            {'c', 3},
+            {'d', 4},
+            {'e', 5},
+            {'f', 6},
+            {'g', 7},
+            {'h', 8},
+            {'i', 9},
+            {'j', 10},
+            {'k', 11},
+            {'l', 12},
+            {'m', 13},
+            {'n', 14},
+            {'o', 15},
+            {'p', 16},
+            {'q', 17},
+            {'r', 18},
+            {'s', 19},
+            {'t', 20},
+            {'u', 21},
+            {'v', 22},
+            {'w', 23},
+            {'x', 24},
+            {'y', 25},
+            {'z', 26}
+        };
+
+        public static int GetLetterValue(char letter)
+        {
+            //Return the value of the letter if it is in the dictionary, otherwise return 0
+            return _alphabet.ContainsKey(letter) ? _alphabet[letter] : 0;
+        }
+    }
+}
---
+++ Letter Value Sum/Program.cs
@@ -0,0 +1,16 @@
+﻿//The purpose of this challenge is to assign every letter of the alphabet a value from 1 to 26. When typing a word, phrase, etc, the value of each character is added together. If the character is not a letter such as a number, the value is ignored.
+//For example, the word hello would have a value of 52 because h is 8, e is 5, l is 12, and o is 15. The word HELLO would also have a value of 52 because the letters are the same, only the case is different. This is case sensitive so the word Hello would have a value of 37.
+//The challenge is to write a program that determines the value of a word or phrase entered as input.
+
+using Letter_Value_Sum;
+
+Console.WriteLine("Enter a word or phrase to get the value of each letter added together:");
+string input = Console.ReadLine()!.ToLower();
+
+int sum = 0;
+foreach (char letter in input)
+{
+    sum += Alphabet.GetLetterValue(letter);
+}
+
+Console.WriteLine($"\nSum of your input '{input}' is {sum}");
